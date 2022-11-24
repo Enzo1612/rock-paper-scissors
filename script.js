d96bb805a4700b7077cdca4d310329dd7a8805fc
Enzo <113211035+Enzo1612@users.noreply.github.com>
2022-11-24 20:35:46 +0100

    add the beginning of the interface

--- script.js
+++ script.js
@@ -13,57 +13,101 @@ function getComputerChoice() {
 let playerScore = 0
 let computerScore = 0
 function score () {
-    return "Player win: " + playerScore + " Computer win: " + computerScore;
+    gameScore.textContent = "Player win: " + playerScore + " Computer win: " + computerScore;
 }
+//create 3 buttons for each choices//
+
+//rock//
+let rock = document.createElement('button')
+rock.textContent = 'rock'
+rock.classList.add('rock', 'choiceButton');
+document.body.appendChild(rock);
+//paper//
+let paper = document.createElement('button')
+paper.textContent = 'paper'
+paper.classList.add('paper', 'choiceButton');
+document.body.appendChild(paper);
+//scissors//
+let scissors = document.createElement('button')
+scissors.textContent = 'scissors'
+scissors.classList.add('scissors', 'choiceButton');
+document.body.appendChild(scissors);
+//addEventListener to buttons//
+const choiceButtons = document.querySelectorAll('.choiceButton');
+choiceButtons.forEach(button => button.addEventListener('click', () => {
+
+    playerChoice = button.textContent;
+    rockPaperScissors();
+    playRound();
+
+}))
+//create a div element to display results//
+let div = document.createElement('div');
+div.classList.add('div');
+div.textContent = 'ROCK PAPER SCISSORS'
+document.body.appendChild(div);
+//create a p element to display result of the round in div//
+let roundResult = document.createElement('p');
+roundResult.textContent = "";
+div.appendChild(roundResult);
+//create a p element to display the word 'score'//
+let scoreP = document.createElement('p');
+scoreP.textContent = "Score:";
+div.appendChild(scoreP);
+//create a p element to display the score//
+let gameScore = document.createElement('p');
+gameScore.textContent = "";
+div.appendChild(gameScore);
 //Single round of rock paper scissors//
-function rockPaperScissors () {
-    let playerChoice = prompt("Enter 0 for Rock, 1 for Paper or 2 for Scissors: ")
-    playerChoice = choices[playerChoice];
-    let computerChoice = getComputerChoice();
-    if (playerChoice ==  computerChoice){
-        return "It's a tie, you both picked " + computerChoice;
+    function rockPaperScissors() {
+        let computerChoice = getComputerChoice();
+        if (playerChoice ==  computerChoice){
+            roundResult.textContent = `It's a tie, you both picked ${computerChoice}`;
+        }
+        else if (computerChoice == "rock" && playerChoice == "scissors"){
+            ++computerScore;
+            roundResult.textContent = "Computer win... He picked rock and you picked scissors..."
+        }
+        else if (computerChoice == "scissors" && playerChoice == "rock"){
+            ++playerScore;
+            roundResult.textContent = "You win! You picked scissors and he picked rock..."
+        }
+        else if (computerChoice == "rock" && playerChoice == "paper"){
+            ++playerScore;
+            roundResult.textContent = "You win! You picked paper and he picked rock..."
+        }
+        else if (computerChoice == "scissors" && playerChoice == "paper"){
+            ++computerScore;
+            roundResult.textContent = "Computer win... He picked scissors and you picked paper..."
+        }
+        else if (computerChoice == "paper" && playerChoice == "scissors"){
+            ++playerScore
+            roundResult.textContent = "You win! You picked scissors and he picked paper..."
+        }
+        else if (computerChoice == "paper" && playerChoice == "rock"){
+            ++computerScore
+            roundResult.textContent = "Computer win... He picked paper and you picked rock..."
+        }
+        else {
+            roundResult.textContent = "Unknown variable entered... Try again!";
+        }
     }
-    else if (computerChoice == "rock" && playerChoice == "scissors"){
-        ++computerScore;
-        return "Computer win... He picked rock and you picked scissors..."
-    }
-    else if (computerChoice == "scissors" && playerChoice == "rock"){
-        ++playerScore;
-        return "You win! You picked scissors and he picked rock..."
-    }
-    else if (computerChoice == "rock" && playerChoice == "paper"){
-        ++playerScore;
-        return "You win! You picked paper and he picked rock..."
-    }
-    else if (computerChoice == "scissors" && playerChoice == "paper"){
-        ++computerScore;
-        return "Computer win... He picked scissors and you picked paper..."
-    }
-    else if (computerChoice == "paper" && playerChoice == "scissors"){
-        ++playerScore
-        return "You win! You picked scissors and he picked paper..."
-    }
-    else if (computerChoice == "paper" && playerChoice == "rock"){
-        ++computerScore
-        return "Computer win... He picked paper and you picked rock..."
-    }
-    else {
-        console.log("Unknown variable entered... Try again!");
-    }
-}
-//Loop repeating 5 times in order to play 5 round of rock paper scissors//
-for (let i = 0; i < 6; i++){
-    if (computerScore !== 3 && playerScore !==3) {
-        console.log(rockPaperScissors(),`
-        `,score());
-    }
-    else if (playerScore == 3) {
-        console.log("Well played, you won!!!");
-    }
-    else if (computerScore == 3) {
-        console.log("You lost... Try again...");
-    }
-    if (computerScore == playerScore) {
-        i--;
-    }
-}
+        //Loop repeating 5 times in order to play 5 round of rock paper scissors//
+        function playRound(){
+                if (computerScore !== 3 && playerScore !==3) {
+                    rockPaperScissors(),`
+                    `,score();
+                }
+                if (playerScore == 3) {
+                    gameScore.textContent = "Well played, you won!!!";
+                    playerScore = 0
+                    computerScore = 0
+                }
+                else if (computerScore == 3) {
+                    gameScore.textContent = "You lost... Try again...";
+                    computerScore = 0
+                    playerScore = 0
+                }
+            }
+
+    ////playRound()
